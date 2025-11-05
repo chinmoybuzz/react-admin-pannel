ff7fd1c18511923be7c63fee2c8d607eaf228df1
chinmoybuzz <chinmoybuzz@gmail.com>
2025-11-05 16:09:07 +0530

    testing 9

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,17 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-const Layout = ({ children }) => {
+import { Outlet } from "react-router-dom";
+const Layout = () => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 bg-gray-50 p-6  dark:bg-gray-900 min-h-screen transition-colors">
         <Navbar />
 
-        <main className="p-6 flex-1 text-gray-900 dark:text-gray-100 transition-colors">{children}</main>
+        <main className="p-6 flex-1 text-gray-900 dark:text-gray-100 transition-colors">
+          {/* {children} */}
+          <Outlet />
+        </main>
       </div>
     </div>
   );
